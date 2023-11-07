6850cb308c23b1da724f203c08c4b9c2f4272c26
Kevin Li <87391349+Martin101TO@users.noreply.github.com>
2023-11-07 13:40:58 -0700

    Update HuffmanArrayWord.java
    started basic methods

--- src/HuffmanArrayWord.java
+++ src/HuffmanArrayWord.java
@@ -1,4 +1,28 @@
 
 public class HuffmanArrayWord {
 
+    public void HuffmanArrayWord(String inputFileName, int cmd) {
+        if (cmd == 0) {
+            encode(inputFileName, inputFileName + ".huf");
+        } else if (cmd == 1) {
+            decode(inputFileName, inputFileName + ".dec");
+        } else {
+            System.out.println("Invalid command");
+        }
+    }
+
+    public void encode(String inputFileName, String outputFileName) {
+        String array[] = new String[1000];
+        
+    }
+
+    public void decode(String inputFileName, String outputFileName) {
+
+    }
+
+    public void printFrequencyTable() {
+
+    }
+
+
 }
