d97d861ec7c350844a77931cf9ebfac3516527fb
Kevin Li <87391349+Martin101TO@users.noreply.github.com>
2023-11-18 02:28:46 -0700

    Functional encoding, cannot save spaces and newlines

--- src/HuffmanArrayLetter.java
+++ src/HuffmanArrayLetter.java
@@ -1,8 +1,4 @@
 
 public class HuffmanArrayLetter {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
 
-        HuffmanArrayLetter huffman = new HuffmanArrayLetter("input.txt", 0);
-    }
 }
