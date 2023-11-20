26b7652a14f0763e453dd5dc304172e8ddc56bf0
Kevin Li <87391349+Martin101TO@users.noreply.github.com>
2023-11-20 15:36:02 -0700

    Optimized huffmanarrayword encoding

--- src/HuffmanArrayWordTest.java
+++ src/HuffmanArrayWordTest.java
@@ -1,26 +1,23 @@
-import javax.swing.event.HyperlinkEvent;
-
 public class HuffmanArrayWordTest {
     
     public static void main(String[] args) {
         
         HuffmanArrayWord huffman;
-        huffman = new HuffmanArrayWord("src\\testHffArrWrdTest.txt", 0);
+        huffman = new HuffmanArrayWord("testHffArrWrdTest.txt", 0);
         // huffman.printWordMap();
         // huffman.printPQ();
         huffman.printEncodeMap();
 
         HuffmanArrayWord huffman2;
-        huffman2 = new HuffmanArrayWord("src\\testHffArrWrdTest.txt.huf", 1);
+        huffman2 = new HuffmanArrayWord("huf_testHffArrWrdTest.txt", 1);
         huffman2.printEncodeMap();
-
         
         HuffmanArrayWord beeMovie;
-        beeMovie = new HuffmanArrayWord("src\\testBeeMovie.txt", 0);
-        beeMovie.printEncodeMap();
+        // beeMovie = new HuffmanArrayWord("testBeeMovie.txt", 0);
+        // beeMovie.printEncodeMap();
 
         HuffmanArrayWord macbeth;
-        macbeth = new HuffmanArrayWord("src\\testMacbeth.txt", 0);
-        macbeth.printEncodeMap();
+        // macbeth = new HuffmanArrayWord("testMacbeth.txt", 0);
+        // macbeth.printEncodeMap();
     }
 }   
